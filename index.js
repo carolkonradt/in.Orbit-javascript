89acc9b866dfc78df9882d03610d7a2109429e62
carolkonradt <carolkonradt41@gmail.com>
2024-09-12 12:39:54 -0300

    metas abertas

--- index.js
+++ index.js
@@ -57,6 +57,22 @@ const metasRealizadas = async () => {
     })
 }
 
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta)=>{
+        return meta.checked != true;
+    })
+
+    if(abertas.length==0){
+        console.log("Não existem metas abertas :)");
+        return;
+    }
+
+    await select({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
+    })
+}
+
 const start = async() => {
     console.log("Começou")
     while(true){
@@ -75,6 +91,10 @@ const start = async() => {
                 {
                     name: "Metas Realizadas",
                     value: "realizadas"
+                },
+                {
+                    name: "Metas Abertas",
+                    value: "abertas"
                 },
                 {
                     name: "Sair",
@@ -100,6 +120,10 @@ const start = async() => {
                 
                     case "realizadas":
                         await metasRealizadas();
+                        break;
+
+                    case "abertas":
+                        await metasAbertas();
                         break;
         }
     }
