a4a7f0d075e5e83449f791cbdc7f9dab5cc80d4b
carolkonradt <carolkonradt41@gmail.com>
2024-09-12 13:40:36 -0300

    deletar metas

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 }
@@ -71,6 +71,31 @@ const metasAbertas = async () => {
         message: "Metas Abertas: " + abertas.length,
         choices: [...abertas]
     })
+}
+
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return {value: meta.value, checked: false}
+    })
+
+    const itensADeletar = await checkbox({
+        message: "Selecione item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false
+    })
+
+    if(itensADeletar.length == 0){
+        console.log("Nenhum item para deletar");
+        return;
+    }
+
+    itensADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+           return meta.value != item;
+        })
+    })
+    console.log("Meta(s) deletada(s) com sucesso!");
+    
 }
 
 const start = async() => {
@@ -97,6 +122,10 @@ const start = async() => {
                     value: "abertas"
                 },
                 {
+                    name: "Deletar Metas",
+                    value: "deletar"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -118,13 +147,17 @@ const start = async() => {
                     await listarMetas();
                     break;
                 
-                    case "realizadas":
-                        await metasRealizadas();
-                        break;
+                case "realizadas":
+                    await metasRealizadas();
+                    break;
 
-                    case "abertas":
-                        await metasAbertas();
-                        break;
+                case "abertas":
+                    await metasAbertas();
+                    break;
+
+                case "deletar":
+                    await deletarMetas();
+                    break;
         }
     }
 }
